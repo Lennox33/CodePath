cf24046d70c32905e14e4c0f18dadfec0152ee3d
Lennox Scholl <ox0235198@gmail.com>
2022-10-09 00:25:47 -0400

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+let themeButton = document.getElementById("theme-button");
+
+const toggleDarkMode = () => {
+
+  // Write your code to manipulate the DOM here
+  document.body.classList.toggle("dark-mode");
+  themeButton.addEventListener("click", toggleDarkMode);
+
+}
