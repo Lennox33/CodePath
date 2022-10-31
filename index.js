802d32ca5f76e58beaf71b592ce1593d52189fc8
Lennox Scholl <ox0235198@gmail.com>
2022-10-31 19:06:37 -0400

    init

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 let themeButton = document.getElementById("theme-button");
 
 const toggleDarkMode = () => {
-  // Write your code to manipulate the DOM here
   document.body.classList.toggle("dark-mode");
 }
 
 themeButton.addEventListener("click", toggleDarkMode);
+
+// Petition form
+
+var goals = document.querySelector("sign-now-button");
+
+const addSignature = (name , hometown) => {
+  // Write your code to manipulate the DOM here
+  console.log(name);
+  event.preventDefault();
+}
+
+// Add a click event listener to the sign now button here
+form.addEventListener("submit", addSignature);
