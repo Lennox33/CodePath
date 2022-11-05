7de71369f9cf6ab76307329e7c55848251d7669e
Lennox Scholl <ox0235198@gmail.com>
2022-11-05 15:13:02 -0400

    init

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Toggle dark mode
 let themeButton = document.getElementById("theme-button");
 
 const toggleDarkMode = () => {
@@ -5,16 +6,91 @@ const toggleDarkMode = () => {
 }
 
 themeButton.addEventListener("click", toggleDarkMode);
+//End toggle dark mode
 
 // Petition form
+let signNowButton = document.getElementById('sign');
 
-var goals = document.querySelector("sign-now-button");
-
-const addSignature = (name , hometown) => {
-  // Write your code to manipulate the DOM here
-  console.log(name);
+const addSignature = () => {
+  var name = document.getElementById('name').value;
+  var hometown = document.getElementById('hometown').value;
+  console.log(name);  document.getElementById("signatures").innerHTML = ("🖊️ " + name + " from "+ hometown + " supports this.");
   event.preventDefault();
 }
 
-// Add a click event listener to the sign now button here
-form.addEventListener("submit", addSignature);
+const validateForm = () => {
+
+  let containsErrors = false;
+
+  let petitionInputs = document.getElementById("sign-petition").elements;
+
+  for (let i = 0; i < petitionInputs.length; i++) {
+    
+    if (petitionInputs[i].value.length < 2) {
+      petitionInputs[i].classList.add('error');
+      containsErrors = true;
+    }
+    else {
+      petitionInputs[i].classList.remove('error');
+    }
+  }
+  if (containsErrors == false) {
+    addSignature();
+    for(let i = 0; i < petitionInputs.length; i++){
+      petitionInputs[i].value = "";
+      containsErrors = false;
+    }
+  }
+}
+signNowButton.addEventListener('click', validateForm);
+//End petitions form
+
+//Start news api
+const apiKey = 'NLaNBUKGLKf9G0QtdUI-aXToTTrQA8dv4db8a4USVzVX1YRK';
+const keywords = 'physical health';
+const url = 'https://api.currentsapi.services/v1/search?apiKey=' + apiKey + '&keywords=' + keywords;
+
+const getAndDisplayNews = async () => {
+  // Fetches News articles
+const response = await fetch(url);
+
+// Contains JavaScript objects with news articles
+const data = await response.json();
+
+// Parses the data and assigns the news article array from the objects
+const news = data.news;
+
+  for(let i = 0; i < 6; i++){
+    const title = document.createElement('h3');
+    title.textContent = news[i].title;
+    
+    const description = document.createElement('p');
+    description.textContent = news[i].description;
+
+    const article = document.createElement('article');
+    article.appendChild(title);
+    article.appendChild(description);
+
+    const newsDiv = document.getElementById('news');
+    newsDiv.append(article);
+  }
+  
+}
+//End news api
+
+//Books section
+const proxyURL = "https://cp-proxy5.herokuapp.com/";
+
+const getBooks = () => {
+  const olQueryURL = "https://openlibrary.org/works/OL3004974W/Concepts_of_Physical_Fitness?edition=ia%3Aconceptsofphysic0000corb_j5b4";
+  const bookId = "OL3004974W";
+  const exerciseBookURL = "https://cp-proxy5.herokuapp.com/https://openlibrary.org/works/" + "OL3004974W" + ".json";
+
+fetch('https://openlibrary.org/works/OL3004974W/Concepts_of_Physical_Fitness?edition=ia%3Aconceptsofphysic0000corb_j5b4')
+  .then((response) => response.json())
+  .then((data) => {
+    // more code here later
+    console.log(data)
+  });
+}
+getBooks();
